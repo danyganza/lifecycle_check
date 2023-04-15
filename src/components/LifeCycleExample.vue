299c2c3619915a02cc83554df1bacae77538f9eb
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-15 11:28:40 -0600

    added

---
+++ src/components/LifeCycleExample.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+<h1>hello</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        beforeCreate: function (){
+            console.log(`hello`)
+        },
+        created: function (){
+            console.log(`hello there`)
+        },
+        beforeMount: function (){
+            console.log(`hello hi`)
+        },
+        mounted : function (){
+            console.log(`hell hello`)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
